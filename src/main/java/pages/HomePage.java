d13875e5a144156fa335f733f23a844ae04e4098
shajigholam <shajigholam@myseneca.ca>
2024-10-04 15:10:10 -0400

    using screenshots in the home page when the test fails

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,10 +3,13 @@ package pages;
 import drivers.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Constants;
+import utils.Utils;
 
 import java.time.Duration;
 
@@ -44,5 +47,26 @@ public class HomePage {
         wait.until(ExpectedConditions.visibilityOf(username));
         return username.getText();
     }
+
+    /*
+    // to use screenshot method when there is a sth wrong
+    public void addFirstElementToCart() {
+        Actions hover = new Actions(driver);
+        hover.moveToElement(firstElement).build().perform();
+        addToCartFirst.click();
+        WebDriverWait wait = new WebDriverWait(driver, Constants.TIMEOUT);
+        wait.until(ExpectedConditions.elementToBeClickable(continueShoppingButton));
+        continueShoppingButton.click();
+        if (cart.getText().contains(Constants.CART_QUANTITY)) {
+            System.out.println("cart has been updated");
+            Utils.takeScreenshot();
+        }
+        else {
+            System.out.println("cart has not been updated");
+
+        }
+    }
+
+     */
 
 }
