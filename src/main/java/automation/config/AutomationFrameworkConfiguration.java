8dca4968221e02120de78a3df82c31cf3a4f88cd
shajigholam <shajigholam@myseneca.ca>
2024-10-04 17:58:20 -0400

    refactoring

--- src/main/java/automation/config/AutomationFrameworkConfiguration.java
+++ src/main/java/automation/config/AutomationFrameworkConfiguration.java
@@ -1,11 +1,12 @@
-//*** we need this class to scan our framework for components and will create some instances of the components in order to inject them further in the classes
-package config;
+//*** we need this class to scan our framework for components and will create some instances of the components
+// in order to inject them further in the classes
+package automation.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("automation")
+@ComponentScan("main")
 public class AutomationFrameworkConfiguration {
     public AutomationFrameworkConfiguration(){}
 }
--- src/main/java/drivers/strategies/DriverStrategy.java
+++
@@ -1,7 +0,0 @@
-package drivers.strategies;
-
-import org.openqa.selenium.WebDriver;
-
-public interface DriverStrategy {
-    WebDriver setStrategy();
-}
